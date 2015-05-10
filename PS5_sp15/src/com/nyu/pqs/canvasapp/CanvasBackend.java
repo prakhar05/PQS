79495e60a84e39226207eacea0d26235a057b0fd
Prakhar Verma <pv594@nyu.edu>
2015-05-10 12:25:31 -0400

    AddListener function created

---
+++ PS5_sp15/src/com/nyu/pqs/canvasapp/CanvasBackend.java
@@ -0,0 +1,47 @@
+package com.nyu.pqs.canvasapp;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CanvasBackend implements Model {
+  private List<View> CanvasList = new ArrayList<View>();
+  private int oldX;
+  private int oldY;
+  private int currentX;
+  private int currentY;
+  
+  
+  @Override
+  public void registerListener(View canvas) throws IllegalArgumentException {
+    if(!CanvasList.contains(canvas)){
+      CanvasList.add(canvas);
+    }else{
+      throw new IllegalArgumentException("This canvas already exists in list of listeners");
+    }
+   }
+
+  @Override
+  public void deleteListener(View canvas) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void createNewDrawing() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void updateCanvas() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void selectDrawingObject() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
--- PS5_sp15/src/com/nyu/pqs/canvasapp/View.java
+++ PS5_sp15/src/com/nyu/pqs/canvasapp/View.java
@@ -1,5 +1,9 @@
 package com.nyu.pqs.canvasapp;
 
 public interface View {
-
+  //act as a new drawing or a reset drawing
+  public void createNewDrawing();
+  //update the view with data from model
+  public void updateView();
+  
 }
